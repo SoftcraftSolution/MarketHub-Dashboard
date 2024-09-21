736984cbd397a0e1d55867cff8e8de27dfc4f771
SoftcraftSolution <Rahul@softcraft.in>
2024-09-21 17:10:26 +0530

    login

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './Component/Sidebar/Sidebar'; // Adjust the import path as needed
 import DashboardPage from './Component/UserList/UserList';
+import Login from './Component/Login/login';
 
 function App() {
   return (
     <Router>
       <div style={{ display: 'flex' }}>
-        <Sidebar /> {/* Render the Sidebar */}
+        {/* Render the Sidebar */}
         <div style={{ flex: 1, padding: '20px' }}>
           <Routes>
             <Route path="/userlist" element={<DashboardPage />} />
+            <Route path="/login" element={<Login />} />
             {/* Add more routes here as needed */}
           </Routes>
         </div>
